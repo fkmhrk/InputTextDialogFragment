fb57ed38be2ac94e060d29e430bf07f52c906375
hrk fkm <fkm@fkmsoft.jp>
2015-01-19 16:34:52 +0900

    Rename and shows keyboard

--- v4/lib/src/main/java/jp/fkmsoft/libs/inputtextdialog/InputTextDialogFragment.java
+++ v4/lib/src/main/java/jp/fkmsoft/libs/inputtextdialog/InputTextDialogFragment.java
@@ -1,35 +1,42 @@
-package jp.fkmsoft.libs.progress;
+package jp.fkmsoft.libs.inputtextdialog;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import jp.fkmsoft.libs.inputtextdialog.R;
 
 /**
  * Dialog fragment for displaying Progress
  */
 public class InputTextDialogFragment extends DialogFragment {
+    public static final String EXTRA_DATA = "extra";
+
     private static final String ARGS_TITLE = "title";
     private static final String ARGS_MESSAGE = "message";
     private static final String ARGS_INIT_TEXT = "initText";
     private static final String ARGS_EXTRA = "extra";
-
-    public static final String EXTRA_DATA = "extra";
-
+    private static final String ARGS_INPUT_TYPE = "inputType";
 
     public static InputTextDialogFragment newInstance(Fragment target, int requestCode,
                                                      String title, String message, String initText, Bundle extra) {
+        return newInstance(target, requestCode, title, message, initText, InputType.TYPE_CLASS_TEXT, extra);
+    }
+
+    public static InputTextDialogFragment newInstance(Fragment target, int requestCode,
+        String title, String message, String initText, int inputType, Bundle extra) {
         InputTextDialogFragment fragment = new InputTextDialogFragment();
         fragment.setTargetFragment(target, requestCode);
         
@@ -37,6 +44,7 @@ public class InputTextDialogFragment extends DialogFragment {
         args.putString(ARGS_TITLE, title);
         args.putString(ARGS_MESSAGE, message);
         args.putString(ARGS_INIT_TEXT, initText);
+        args.putInt(ARGS_INPUT_TYPE, inputType);
         args.putBundle(ARGS_EXTRA, extra);
         fragment.setArguments(args);
         
@@ -49,12 +57,13 @@ public class InputTextDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Activity activity = getActivity();
-        if (activity == null) { return null; }
+        if (activity == null) { return super.onCreateDialog(savedInstanceState); }
 
         Bundle args = getArguments();
         String title = args.getString(ARGS_TITLE);
         String message = args.getString(ARGS_MESSAGE);
         String initText = args.getString(ARGS_INIT_TEXT);
+        int inputType = args.getInt(ARGS_INPUT_TYPE);
 
         LayoutInflater inflater = LayoutInflater.from(activity);
         View root = inflater.inflate(R.layout.lib_dialog_input_text, null);
@@ -62,6 +71,7 @@ public class InputTextDialogFragment extends DialogFragment {
         messageText.setText(message);
         mEdit = (EditText) root.findViewById(R.id.edit);
         mEdit.setText(initText);
+        mEdit.setInputType(inputType);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle(title);
@@ -70,6 +80,15 @@ public class InputTextDialogFragment extends DialogFragment {
         builder.setNegativeButton(android.R.string.cancel, mClickListener);
 
         return builder.create();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Activity activity = getActivity();
+        if (activity == null) { return; }
+        InputMethodManager manager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        manager.showSoftInput(mEdit, InputMethod.SHOW_FORCED);
     }
 
     @Override
